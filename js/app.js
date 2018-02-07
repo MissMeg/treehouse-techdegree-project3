46ebb65b7c74875db71a3c65a902fe3b59952ab8
Miss Pylons <megmrob@gmail.com>
2018-02-07 20:58:00 +0000

    changed regex comment

--- js/app.js
+++ js/app.js
@@ -224,7 +224,7 @@ $(document).ready(() => {
         $('.emailVal').remove();
         //get the email input value
         let emailVal = $('#mail').val();
-        //regex to test against - found on StackOverflow
+        //regex to test against - from emailregex.com
         let test = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         //if nothing is entered - let use know they need to enter an email
         if( emailVal === '') {
